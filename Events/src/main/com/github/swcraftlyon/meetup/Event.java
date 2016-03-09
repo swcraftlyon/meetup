56073a0da23477037d2f4c22328aa8cb54c1bc71
bravogolfgolf <bggibson@hotmail.com>
2016-03-09 19:49:42 +0100

    feat (event) Adds Event Constructor, Parameter, Field and Method
    - Constructor takes String as parameter and assigns it to title
    
    - getTitle() returns title as String

--- Events/src/main/com/github/swcraftlyon/meetup/Event.java
+++ Events/src/main/com/github/swcraftlyon/meetup/Event.java
@@ -1,4 +1,13 @@
 package com.github.swcraftlyon.meetup;
 
 public class Event {
+    private String title;
+
+    public Event(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
