b7e351002711b80ada557941ac565bc31ebad81a
bravogolfgolf <bggibson@hotmail.com>
2016-03-18 01:18:25 +0100

    feat (user) Add User and UserTest Classes
    --Constructor
    ----Initially sets username
    --Getter for username

---
+++ Events/src/main/java/com/github/swcraftlyon/meetup/User.java
@@ -0,0 +1,13 @@
+package com.github.swcraftlyon.meetup;
+
+public class User {
+    private final String userName;
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
